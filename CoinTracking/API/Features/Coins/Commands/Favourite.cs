8039c937fe5d81d869c7c4090f7d57422205c52a
TuDang <71314673+UIT-AnhTu-0325@users.noreply.github.com>
2022-11-24 23:03:09 +0700

    Update favorite api

--- CoinTracking/API/Features/Coins/Commands/Favourite.cs
+++ CoinTracking/API/Features/Coins/Commands/Favourite.cs
@@ -24,10 +24,10 @@ namespace API.Features.Coins.Commands
             {
                 var coin = await _dbContext.Coins
                     .Include(x => x.Users)
-                    .Where(x => x.Id == command.Request.CoinId)
-                    .FirstOrDefaultAsync();
+                    .Where(x => x.Id == command.CoinId)
+                    .FirstOrDefaultAsync(cancellationToken: cancellationToken);
                 var user = await _dbContext.Users.Where(x => x.Id == command.Request.UserId)
-                    .FirstOrDefaultAsync();
+                    .FirstOrDefaultAsync(cancellationToken: cancellationToken);
 
                 if (coin == null || user == null)
                 {
@@ -46,6 +46,9 @@ namespace API.Features.Coins.Commands
 
         public class Command : BaseCommand, IRequest<OperationResult>
         {
+            [Required]
+            [FromRoute]
+            public Guid CoinId { get; set; }
             [FromBody]
             [Required]
             public Request Request { get; set; } = default!;
@@ -55,7 +58,6 @@ namespace API.Features.Coins.Commands
         {
             [Required]
             public Guid UserId { get; set; }
-            public Guid CoinId { get; set; }
         }
     }
 }
